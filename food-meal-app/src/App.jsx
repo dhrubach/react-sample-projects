cfc49cf4aa2c68518e8300ca18a79503b24af2e5
Dhruba Chatterjee <dhruba.chatterjee@fiserv.com>
2021-06-10 23:58:37 +0530

    update : cart button component

--- food-meal-app/src/App.jsx
+++ food-meal-app/src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import Header from "./components/UI/Header";
+import Header from "./components/Layout/Header";
 
 function App() {
 	return (
--- food-meal-app/src/components/Layout/Header.jsx
+++ food-meal-app/src/components/Layout/Header.jsx
@@ -2,13 +2,14 @@ import React from "react";
 
 import heroImage from "./../../assets/meals.jpg";
 import styles from "./Header.module.css";
+import HeaderCartButton from "./HeaderCartButton";
 
 const Header = (props) => {
 	return (
 		<React.Fragment>
 			<header className={styles.header}>
 				<h1>Sample Food Order App</h1>
-				<button>Cart</button>
+				<HeaderCartButton />
 			</header>
 			<div className={styles["main-image"]}>
 				<img src={heroImage} alt="A food table" />
