4c223288027c611dab2a02ebe44f65a69a4ac1d2
Dhruba Chatterjee <dhruba.chatterjee@fiserv.com>
2021-06-15 22:35:22 +0530

    update : cart modal using portal

---
+++ food-meal-app/src/components/UI/Modal.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import styles from "./Modal.module.scss";
+
+const Backdrop = () => {
+	return <div className={styles.backdrop}></div>;
+};
+
+const ModalOverlay = (props) => {
+	return (
+		<div className={styles.modal}>
+			<div className={styles.content}>{props.children}</div>
+		</div>
+	);
+};
+
+const portalElement = document.getElementById("overlays");
+
+const Modal = (props) => {
+	return (
+		<React.Fragment>
+			{ReactDOM.createPortal(<Backdrop />, portalElement)}
+			{ReactDOM.createPortal(
+				<ModalOverlay>{props.children}</ModalOverlay>,
+				portalElement
+			)}
+		</React.Fragment>
+	);
+};
+
+export default Modal;
