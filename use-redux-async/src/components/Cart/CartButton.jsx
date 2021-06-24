6eabd14fa92c7ab0b99385fc43c087678abd0f41
Dhruba Chatterjee <dhruba.chatterjee@fiserv.com>
2021-06-24 23:11:03 +0530

    update : total cart items badge

--- use-redux-async/src/components/Cart/CartButton.jsx
+++ use-redux-async/src/components/Cart/CartButton.jsx
@@ -1,10 +1,14 @@
+import { useSelector } from "react-redux";
+
 import styles from "./CartButton.module.scss";
 
 const CartButton = (props) => {
+	const totalItems = useSelector((state) => state.cart.totalItems);
+
 	return (
 		<button className={styles.button} onClick={props.onClick}>
 			<span>My Cart</span>
-			<span className={styles.badge}>1</span>
+			<span className={styles.badge}>{totalItems}</span>
 		</button>
 	);
 };
