63d765eb2e9a63733685af4176e29913cd87d8bb
Dhruba Chatterjee <dhrubach@hotmail.com>
2021-06-29 23:29:06 +0530

    update : wildcard route

--- use-react-router/src/App.jsx
+++ use-react-router/src/App.jsx
@@ -3,6 +3,7 @@ import { Redirect, Route, Switch } from "react-router-dom";
 import { Layout } from "./components/layout/Layout";
 import { AllQuotes } from "./pages/AllQuotes";
 import { NewQuote } from "./pages/NewQuote";
+import { NotFound } from "./pages/NotFound";
 import { QuoteDetail } from "./pages/QuoteDetail";
 
 function App() {
@@ -20,6 +21,9 @@ function App() {
 				</Route>
 				<Route path="/add-quote">
 					<NewQuote />
+				</Route>
+				<Route path="*">
+					<NotFound />
 				</Route>
 			</Switch>
 		</Layout>
---
+++ use-react-router/src/pages/NotFound.jsx
@@ -0,0 +1,5 @@
+const NotFound = () => {
+	return <p className="centered">Not Found !!</p>;
+};
+
+export { NotFound };
