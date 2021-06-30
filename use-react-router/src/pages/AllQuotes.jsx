1da7e2c17d578fd3ecb56b71155aa400bd018215
Dhruba Chatterjee <dhrubach@hotmail.com>
2021-06-30 20:07:39 +0530

    update : fetch all quotes from firebase

--- use-react-router/src/pages/AllQuotes.jsx
+++ use-react-router/src/pages/AllQuotes.jsx
@@ -1,20 +1,40 @@
-import { QuoteList } from "./../components/quotes/QuoteList";
+import { useEffect } from "react";
 
-const DUMMY_QUOTES = [
-	{
-		id: "q1",
-		author: "John Doe",
-		text: "First Quote",
-	},
-	{
-		id: "q2",
-		author: "Jane Doe",
-		text: "Second Quote",
-	},
-];
+import { QuoteList } from "./../components/quotes/QuoteList";
+import { LoadingSpinner } from "./../components/ui/LoadingSpinner";
+import { NoQuotesFound } from "./../components/quotes/NoQuotesFound";
+import { useHttp } from "./../hooks/use-http";
+import { getAllQuotes } from "./../lib/api";
 
 const AllQuotes = () => {
-	return <QuoteList quotes={DUMMY_QUOTES}></QuoteList>;
+	const {
+		sendRequest,
+		status,
+		data: loadedQuotes,
+		error,
+	} = useHttp(getAllQuotes, true);
+
+	useEffect(() => {
+		sendRequest();
+	}, [sendRequest]);
+
+	if (status === "pending") {
+		return (
+			<div className="centered">
+				<LoadingSpinner />
+			</div>
+		);
+	}
+
+	if (status === "error") {
+		return <p className="centered focused">{error}</p>;
+	}
+
+	if (status === "complete" && (!loadedQuotes || loadedQuotes.length === 0)) {
+		return <NoQuotesFound />;
+	}
+
+	return <QuoteList quotes={loadedQuotes}></QuoteList>;
 };
 
 export { AllQuotes };
