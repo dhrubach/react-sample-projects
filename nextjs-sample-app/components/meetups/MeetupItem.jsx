13c0a0a04a416b168b5ad7394473e4b54780591e
Dhruba Chatterjee <dhrubach@hotmail.com>
2021-07-01 23:41:21 +0530

    update : include meetup components

---
+++ nextjs-sample-app/components/meetups/MeetupItem.jsx
@@ -0,0 +1,23 @@
+import { Card } from "../ui/Card";
+import styles from "./MeetupItem.module.css";
+
+function MeetupItem(props) {
+	return (
+		<li className={styles.item}>
+			<Card>
+				<div className={styles.image}>
+					<img src={props.image} alt={props.title} />
+				</div>
+				<div className={styles.content}>
+					<h3>{props.title}</h3>
+					<address>{props.address}</address>
+				</div>
+				<div className={styles.actions}>
+					<button>Show Details</button>
+				</div>
+			</Card>
+		</li>
+	);
+}
+
+export { MeetupItem };
---
+++ nextjs-sample-app/components/meetups/MeetupList.jsx
@@ -0,0 +1,20 @@
+import { MeetupItem } from "./MeetupItem";
+import styles from "./MeetupList.module.css";
+
+function MeetupList(props) {
+	return (
+		<ul className={styles.list}>
+			{props.meetups.map((meetup) => (
+				<MeetupItem
+					key={meetup.id}
+					id={meetup.id}
+					image={meetup.image}
+					title={meetup.title}
+					address={meetup.address}
+				/>
+			))}
+		</ul>
+	);
+}
+
+export { MeetupList };
